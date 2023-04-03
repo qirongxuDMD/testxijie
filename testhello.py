08a81c8342f20c9293ee062dc0110c5b7f9d6a93
PC <15062872214@163.com>
2023-04-03 14:19:26 +0800

    测试2

--- testhello.py
+++ testhello.py
@@ -1,1 +1,36 @@
-print("hello33322")
+import keyword
+
+print(keyword.kwlist)
+
+
+def code(self, expect):
+    """
+
+    :param self:
+    :param expect:
+    :return:
+    """
+
+
+# 元组
+x = (1, 2)
+print("x:", type(x))
+# 数字，字符串，列表，元组，字典，集合
+# 列表
+y = ['1', '2', '3']
+print("y:", {type(y)})
+# 字符串
+z = "hello world"
+print("z:", type(z))
+
+# 集合
+j = {"1", "2", "3"}
+print("j:", type(j))
+
+# 字典
+d = {"1": "小米11", "2": "huawei"}
+print("y:", type(d))
+
+# number
+n = 11
+print("y:", type(n))
